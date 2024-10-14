6fd496ece62e85f43047c876e9c6da2414a6b566
Fernando Mare <fernando.mare03@gmail.com>
2024-10-14 20:39:36 -0300

    Routes

---
+++ src/routes/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
